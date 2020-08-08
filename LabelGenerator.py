cd29d223b974c981a814497d5a8df8dd28c7e968
MilesBorealis <64486112+MilesBorealis@users.noreply.github.com>
2020-08-08 10:13:49 -0400

    Update and rename Todoist.py to LabelGenerator.py

--- LabelGenerator.py
+++ LabelGenerator.py
@@ -5,10 +5,10 @@ from todoist.api import TodoistAPI
 #Set api token below.
 api = TodoistAPI('')
 
-#add label for 'Due'
+#create label for 'Due'
 label = api.label.add('Due')
 
-#add labels for months
+#create labels for months
 months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 for x in months:
     """
@@ -17,7 +17,7 @@ for x in months:
     """
     label = api.labels.add(x)
 
-#add labels for dates
+#create labels for dates 1-31
 for y in range(1,32):
     """
     To changes colors of labels, add ", color = XX" after str(y). Refer to Todoist RestAPI for color codes and replace XX.
